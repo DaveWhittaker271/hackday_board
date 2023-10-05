881b2fa08b2979676ec38336cefd3c89fee7e982
Dave Whittaker <davew271@gmail.com>
2023-10-05 11:45:50 +0100

    Store users ID on login

--- src/stores/user.js
+++ src/stores/user.js
@@ -1,9 +1,13 @@
-import {defineStore} from "pinia";
+import {defineStore, storeToRefs} from "pinia";
 
 export const getUserStore = defineStore('user', {
   state: () => ({
+    logged_in: false,
+    id: null,
     name: '',
     email: '',
     picture_url: '',
   }),
 });
+
+export const reactiveUserStore = storeToRefs(getUserStore());

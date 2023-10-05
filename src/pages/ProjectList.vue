9628ef6c001bfc1300fe960388db5aafc3dd4429
Dave Whittaker <davew271@gmail.com>
2023-10-05 13:30:58 +0100

    Link project cards to vue route

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -1,21 +1,22 @@
 <template>
-    <q-page class="flex">
-      <template
-          v-for="(project, index) in projects"
-          :key="index"
-      >
-        <ProjectCard
-          :title="project.title"
-          :description="project.description"
-          :submitted-by="project.submitted_by_name"
-        />
-      </template>
-    </q-page>
+  <q-page class="flex">
+    <template
+      v-for="(project, index) in projects"
+      :key="index"
+    >
+      <ProjectCard
+        :project-id="project.id"
+        :title="project.title"
+        :description="project.description"
+        :submitted-by="project.submitted_by_name"
+      />
+    </template>
+  </q-page>
 </template>
 
 <script>
 import { defineComponent } from 'vue';
-import ProjectCard from "layouts/components/ProjectCard.vue";
+import ProjectCard from "pages/projects/ProjectCard.vue";
 
 import getProjectsQuery from "query/get_projects.graphql";
 
